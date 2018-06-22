6c36b210bd7608be1a883a18b992fdb377216ea9
mzkang <0911kmj@naver.com>
2018-06-22 14:59:49 +0900

    제출한 과제

---
+++ hw1_강민지.py
@@ -0,0 +1,20 @@
+# Assignment Number...: 1
+# Student Name........: 강민지
+# File Name...........: hw1_강민지
+# Program Description : 기본적인 자료형, input 함수를 활용하는 과제입니다.
+
+
+
+season = input('What is your favorite season? ') 
+print(season)       #season 변수를 출력하기 위해 print를 사용        
+date = input('Which date were you born? ')
+print(type(date))   #type으로 date의 자료형을 확인하고, 그 결과를 print로 출력
+date = float(date)  #date 변수 자료형을 float으로 변경
+print(type(date))   #type으로 date의 자료형을 확인하고, 그 결과를 print로 출력    
+print('My favorite season is ' + season + '.' +       #+를 사용했기 때문에 띄어쓰기를 맞춰서 씀
+      ' I was born on the ' + str(int(date)) +'th.')  #str(int(date))을 보면 date가 float라서 소수점(.0)이 나오므로
+                                                      #int로 바꿔 소수점을 없애줌
+                                                      #그런데 +는 str만 연결할 수 있기 때문에 date형식을 다시 str로 바꿔서 연결함
+
+
+
