11faf571b872034e7b5391d80ab9c82746d89fbc
mzkang <40379479+mzkang@users.noreply.github.com>
2018-07-04 09:08:48 +0900

    Update 소수판정.py

--- 소수판정.py
+++ 소수판정.py
@@ -12,3 +12,28 @@ try:
 except ValueError:
     print('ValueError: 1보다 큰 양의 정수를 입력하세요.')
 
+
+    
+<교수님 답>
+while True:
+    try:
+       i = int(input('임의의 양의 정수를 입력하세요. '))
+       if not isinstance(i, int) or i <2 :
+            raise ValueError
+    except ValueError :
+        print('ValueError: 1보다 큰 양의 정수를 입력하세요.')
+        continue
+    else:
+        break
+
+
+
+# ----- 소수확인
+for n in range(2, i-1):
+    if i % n == 0:
+        print('{} x {} = {}'.format(n, i//n, i))
+        print('이 숫자는 소수가 아닙니다.')
+        break
+else:
+    print('이 숫자는 소수입니다.')
+    
