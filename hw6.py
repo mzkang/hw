9521f9b7db12a7f8bfd5441d21a76deb1f11ba09
mzkang <40379479+mzkang@users.noreply.github.com>
2018-07-05 19:18:18 +0900

    수정중

--- hw6.py
+++ hw6.py
@@ -8,5 +8,18 @@ def distance(a,b):
     for 
 
 
+    
+    
+    
+    
+def count(n):
+    if n > 0 :
+        print(n)
+        return count(n-1)
+    else :
+        return 'zero!!'
+
+    
+    
 
 area_triangle_ld = lambda h, w: 0.5 * h * w
